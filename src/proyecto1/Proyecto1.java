fe87b6ab8ccf1541701e732e89413c2b62842b45
Administrador <Administrador@172.16.12.116>
2017-09-07 14:45:34 -0600

    Nuevo Proyecto

---
+++ src/proyecto1/Proyecto1.java
@@ -0,0 +1,19 @@
+/*
+ * Prueba de repositorio en GifHub
+ * 
+ * 
+ */
+package proyecto1;
+
+/**
+ *
+ * @LeifChevez
+ */
+public class Proyecto1 {
+
+ 
+    public static void main(String[] args) {
+       System.out.println("Hola Mundo");
+    }
+    
+}
